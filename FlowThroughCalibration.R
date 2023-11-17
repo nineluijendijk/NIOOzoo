a0c22fb6c68057ea4ba4c9799ecfedc9ed3bc46d
nineluijendijk <nineluijendijk@gmail.com>
2023-11-17 14:15:29 +0100

    updated

--- FlowThroughCalibration.R
+++ FlowThroughCalibration.R
@@ -6,8 +6,8 @@ library(ggpmisc)
 
 data <- read_excel(here("data/FlowThroughKalibreren_R.xlsx"))
 
-data <- data %>% mutate("Difference" = WeightAfter - WeightBefore,
-                "Speed" = Difference / (Time/60),
+data <- data %>% mutate("Speed" = WeightAfter - WeightBefore,
+                "Speed" = Speed / (Time/60),
                 "Unit" = "ml/min")
 
 summary <- data %>% group_by(RPM) %>% summarize(SpeedM = mean(Speed),
@@ -22,6 +22,8 @@ summary %>% ggplot(aes(x = RPM, y = SpeedM))+
   labs(y = "Flow in ml/min",
        title = "Calibration curve Flow Through pump")+
   theme_minimal()
+
+# Using increase in volume
 
 Far <- data[data$Distance == "Far",]
 Middle <- data[data$Distance == "Middle",]
@@ -43,3 +45,29 @@ for (i in seq(100, 900, 200)) {
   filtered <- data %>% filter(RPM == i)
   aov(Difference ~ Distance, filtered) %>% summary.aov() %>% print() # p around 0.7
 }
+
+
+# Using flow speed
+
+
+Far <- data[data$Distance == "Far",]
+Middle <- data[data$Distance == "Middle",]
+Close <- data[data$Distance == "Close",]
+Far$Speed %>% shapiro.test() # p = 0.4568
+Middle$Speed %>% shapiro.test() # p = 0.6826
+Close$Speed %>% shapiro.test() # p = 0.879
+
+leveneTest(data$Speed, data$Distance) # p = 0.8358
+leveneTest(data$Speed, data$Glass) # p = 0.9242
+
+aov(Speed ~ Distance, data) %>% summary.aov() # p = 0.793
+aov(Speed ~ Glass, data) %>% summary.aov() # p = 0.876
+
+filtered <- data %>% filter(Glass == "D" | Glass == "H")
+t.test(formula = filtered$Speed ~ filtered$Glass) # p = 0.3038
+
+for (i in seq(100, 900, 200)) {
+  filtered <- data %>% filter(RPM == i)
+  aov(Speed ~ Distance, filtered) %>% summary.aov() %>% print() # p around 0.7
+}
+
