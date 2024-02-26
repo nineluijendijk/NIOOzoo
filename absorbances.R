936baff3c13accfd32e99e6d44bdbf0b35423e6b
nineluijendijk <nineluijendijk@gmail.com>
2024-02-26 15:52:45 +0100

    statistiek begin

--- absorbances.R
+++ absorbances.R
@@ -1,16 +1,14 @@
 library(readxl)
 library(tidyverse)
 library(here)
-library(multcomp)
 
 dataraw <- read_excel(here("data_raw/OnderzoekMedium.xlsx"), sheet = "Absorbances", range = "A1:F61", na = "NA")
 # datatidy <- mutate(dataraw, "MeanAbsorbance" = rowMeans(dataraw[ , c(4:6)], na.rm = TRUE), A1=NULL, A2=NULL, A3=NULL)
 datatidy <- pivot_longer(dataraw, cols = c("A1", "A2", "A3"), names_to = "Sample", values_to = "Absorbance")
 
-
 regression <- read_excel(here("data_raw/Carbon concentration (based on chemostat C).xlsx"), sheet = "Carbon regression line", range = "A1:C9")
 
-model <- lm(`Carbon Content (mg/L)` ~ `Light absorption (λ)`, data = regression)
+model <- lm(`Carbon Content (mg/L)` ~ `Light absorption (λ)`, data = regression) # Calculate regression curve
 
 data <- mutate(datatidy, "Carbon_mgL" = (datatidy$Absorbance *  model$coefficients[[2]] + model$coefficients[[1]]) / 5 )
 
@@ -23,11 +21,25 @@ ggplot(data = data, aes(y = Carbon_mgL, x = factor(Medium, level = level_order),
        title = "Carbon concentration of the medium after two days",
        x = "Medium")
 
-group_by(data, Medium) %>%
+data %>%
+  group_by(Medium) %>%
+  summarise(p.value.sw = shapiro.test(Carbon_mgL)$p.value) # only KM is normally distributed
+
+leveneTest(Carbon_mgL ~ Medium, data = data)[3] # variances arent equal, p = 0.03612178
+
+summary <- group_by(data, Medium) %>%
   summarise(count = n(),
             mean = mean(Carbon_mgL, na.rm = TRUE),
-            sd = sd(Carbon_mgL, na.rm = TRUE))
+            sd = sd(Carbon_mgL, na.rm = TRUE),
+            var = var(Carbon_mgL, na.rm=TRUE))
 
-res.aov <- aov(Carbon_mgL ~ Medium, data = data)
-summary(res.aov)
-TukeyHSD(res.aov)
+max(summary$var) / min(summary$var) # 3.814713, since the max variance is not 4 times larger than the min I will still conduct a one-way ANOVA
+
+res_aov <- aov(Carbon_mgL ~ Medium, data = data)
+summary(res_aov) # P is significant (0.00147 < 0.05), so the different groups are not equal like assumed
+TukeyHSD(res_aov) # significant for KB-ADaM, KM-ADaM, KH-KB
+#ttest <- pairwise.t.test(data$Carbon_mgL, data$Medium,
+                p.adjust.method = "bonf")
+
+#o <- as.numeric(ttest[[3]]) %>% order()
+#as.numeric(ttest[[3]])[o]
