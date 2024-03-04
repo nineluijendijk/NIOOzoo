16b49f44110c6b23356e642c9e6b1f028d776c12
nineluijendijk <nineluijendijk@gmail.com>
2024-03-04 16:33:00 +0100

    counts plot

--- absorbances.R
+++ absorbances.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(tidyverse)
 library(here)
+library(car)
 
 dataraw <- read_excel(here("data_raw/OnderzoekMedium.xlsx"), sheet = "Absorbances", range = "A1:F61", na = "NA")
 # datatidy <- mutate(dataraw, "MeanAbsorbance" = rowMeans(dataraw[ , c(4:6)], na.rm = TRUE), A1=NULL, A2=NULL, A3=NULL)
@@ -21,25 +22,34 @@ ggplot(data = data, aes(y = Carbon_mgL, x = factor(Medium, level = level_order),
        title = "Carbon concentration of the medium after two days",
        x = "Medium")
 
-data %>%
+dataF <- filter(data, Species == "D. pulex")
+  
+dataF %>%
   group_by(Medium) %>%
-  summarise(p.value.sw = shapiro.test(Carbon_mgL)$p.value) # only KM is normally distributed
-
-leveneTest(Carbon_mgL ~ Medium, data = data)[3] # variances arent equal, p = 0.03612178
-
-summary <- group_by(data, Medium) %>%
-  summarise(count = n(),
-            mean = mean(Carbon_mgL, na.rm = TRUE),
-            sd = sd(Carbon_mgL, na.rm = TRUE),
-            var = var(Carbon_mgL, na.rm=TRUE))
-
+  summarise(p.value.sw = shapiro.test(Carbon_mgL)$p.value) 
+  
+leveneTest(Carbon_mgL ~ Medium, data = dataF)[3] # variances arent equal, p = 0.03612178
+  
+  summary <- group_by(dataF, Medium) %>%
+    summarise(count = n(),
+              mean = mean(Carbon_mgL, na.rm = TRUE),
+              sd = sd(Carbon_mgL, na.rm = TRUE),
+              var = var(Carbon_mgL, na.rm=TRUE))
+  
 max(summary$var) / min(summary$var) # 3.814713, since the max variance is not 4 times larger than the min I will still conduct a one-way ANOVA
+  
+res_aov <- aov(Carbon_mgL ~ Medium, data = dataF)
+summary(res_aov)
+TukeyHSD(res_aov)
 
-res_aov <- aov(Carbon_mgL ~ Medium, data = data)
-summary(res_aov) # P is significant (0.00147 < 0.05), so the different groups are not equal like assumed
-TukeyHSD(res_aov) # significant for KB-ADaM, KM-ADaM, KH-KB
+
+
+
+
+
+
 #ttest <- pairwise.t.test(data$Carbon_mgL, data$Medium,
-                p.adjust.method = "bonf")
+#                p.adjust.method = "bonf")
 
 #o <- as.numeric(ttest[[3]]) %>% order()
 #as.numeric(ttest[[3]])[o]
--- counts.R
+++ counts.R
@@ -1,5 +1,32 @@
 library(readxl)
 library(tidyverse)
 library(here)
+library(plotly)
 
-data <- read_excel(here("data_raw/Countingsheet.xlsx"), sheet = "Sheet2")
+dataraw <- read_excel(here("data_raw/Countingsheet.xlsx"), sheet = "Sheet2")
+
+
+
+data <- dataraw %>% mutate("SmallJuvenileAbundance_indL" = (1000 / `SampleVolume(mL)`) * SmallJuvenile,
+                "LargeJuvenileAbundance_indL" = (1000 / `SampleVolume(mL)`) * LargeJuvenile,
+                "AdultAbundance_indL" = (1000 / `SampleVolume(mL)`) * Adult)
+
+summary <- data %>% group_by(Species, Medium) %>% summarize(`Small juvenile` = mean(SmallJuvenileAbundance_indL, na.rm = TRUE),
+                   `Large juvenile` = mean(LargeJuvenileAbundance_indL, na.rm = TRUE),
+                   `Adult` = mean(AdultAbundance_indL, na.rm = TRUE))
+
+plotting <- pivot_longer(summary, cols = c(`Small juvenile`, `Large juvenile`, Adult),
+            names_to = "Age",
+            values_to = "indL")
+
+ggplot(plotting, aes(x = as.numeric(interaction(Species,Medium)), y = indL, fill = Species, color=Age)) + 
+  geom_bar(stat = "identity", size = 0.7) +
+  scale_x_continuous(breaks=c(2.5, 6.5, 10.5, 14.5, 18.5),labels=c("ADaM","GW","KB", "KH", "KM"))+
+  scale_color_manual(values = c("deeppink","darkorange","blue4")) +
+  guides(color = guide_legend(override.aes = list(fill = "white")))+
+  labs(y = "Individuals per liter", 
+       title = "Mean number of individuals per liter",
+       x = "Medium")+
+  theme_minimal()
+
+
