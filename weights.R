b0d3d78da12629f3d91a7132aaa2dadb9968b826
nineluijendijk <nineluijendijk@gmail.com>
2024-03-07 11:40:03 +0100

    weights added

---
+++ weights.R
@@ -0,0 +1,29 @@
+library(readxl)
+library(tidyverse)
+library(here)
+
+dataraw <- read_excel(here("data_raw/Weighingsheet.xlsx"), range = "A1:E61", na = "NA")
+
+data <- dataraw %>% mutate("WeightIncrease_µg" = WeightTotal - WeightFilter)
+
+summary <- data %>% group_by(Medium, Species) %>% summarize(Weight = mean(WeightIncrease_µg, na.rm = TRUE))
+
+ggplot(data = summary, aes(x = Medium, y = Weight, fill = Species))+
+  geom_col(position = "dodge")+
+  theme_minimal()+
+  labs(y = "Weight in µg")
+
+data %>% filter(! Medium == "KM" | ! Species == "D. galeata") %>% group_by(Medium, Species) %>%
+  summarise(p.value.sw = shapiro.test(WeightIncrease_µg)$p.value) 
+
+species <- c("D. pulex", "D. pulicaria", "D. galeata", "D. ambigua")
+
+for (i in species) {
+  print(i)
+  dataF <- data %>% filter(Species == i)
+  print(leveneTest(WeightIncrease_µg ~ Medium, data = dataF)) 
+  
+  res_aov <- aov(WeightIncrease_µg ~ Medium, data = dataF)
+  print(summary(res_aov))
+  print(TukeyHSD(res_aov))
+}
